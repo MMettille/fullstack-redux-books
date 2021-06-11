5e38e6557334d64a474fc805b04eb8ab0e28aafb
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-11 00:02:08 -0500

    Changed up the App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,61 @@
 import BookList from '../BookList/BookList';
 import BookForm from '../BookForm/BookForm';
-
+import {useEffect} from 'react';
 import './App.css';
+import Axios from 'axios';
+import {useDispatch} from 'react-redux';
+import '@fontsource/roboto';
+import { createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#76d275',
+      main: '#43a047',
+      dark: '#00701a',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#5ddef4',
+      main: '#00acc1',
+      dark: '#007c91',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
 
-  // TODO - GET Book List from server
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    getBooks();
+  }, [])
+
+  const getBooks = () => {
+    Axios.get('/books')
+    .then( response => {
+      // this is where we have response.data
+      // this is where we send data to redux (this week)
+      dispatch({
+        type: 'SET_BOOKS',
+        payload: response.data
+      })
+    }).catch(err =>{
+      console.log(err)
+    })
+  }
 
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header><h1>Books w/ Redux!</h1></header>
       <main>
-        <BookForm />
+        <BookForm getBooks={getBooks}/>
         <BookList />
       </main>
     </div>
+    </ThemeProvider>
   );
 }
 
